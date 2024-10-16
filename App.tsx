e3cf83aa102a1dd9bcc71273b8ef7b221ee66058
kenanhasanic <kenanhasanich@gmail.com>
2024-10-17 00:47:23 +0200

    React Navigation added

--- App.tsx
+++ App.tsx
@@ -7,7 +7,11 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -29,31 +33,81 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen({navigation}: any) {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+      }}>
+      <Text>Home Screen</Text>
+
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('DetailsScreen')}
+      />
     </View>
   );
 }
+function DetailsScreen({navigation}: any) {
+  return (
+    <View
+      style={{
+        height: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+      }}>
+      <Text>Details Screen</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+function AboutScreen({navigation}: any) {
+  return (
+    <View
+      style={{
+        height: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+      }}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
+
+const StackA = createNativeStackNavigator();
+
+function StackANavigator() {
+  return (
+    <StackA.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <StackA.Screen name="HomeScreen" component={HomeScreen} />
+      <StackA.Screen name="DetailsScreen" component={DetailsScreen} />
+    </StackA.Navigator>
+  );
+}
+const StackB = createNativeStackNavigator();
+
+function StackBNavigator() {
+  return (
+    <StackB.Navigator
+      initialRouteName="About"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <StackB.Screen name="About" component={AboutScreen} />
+    </StackB.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -63,56 +117,28 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            paddingBottom: 10, // Add padding to the bottom of the tab bar
+            paddingTop: 5, // Optional: Add padding to the top of the tab bar
+            height: 70, // Optional: Increase the height of the tab bar for more spacing
+          },
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={StackANavigator}
+          options={{headerShown: false}}
+        />
+        <Tab.Screen
+          name="StackB"
+          component={StackBNavigator}
+          options={{headerShown: false}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
